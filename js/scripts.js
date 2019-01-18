78f430580fcfd42987f4b5f5eb9bc4e5b00eecca
Omar Kadah <omarmkadah@gmail.com>
2019-01-18 14:00:23 -0800

    it will alert when submit is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-var flavor = $("input:radio[name=class1]:checked").val();
+// var group1 = $("input:radio[name=group1]:checked").val();
+// var group2 = $("input:radio[name=group2]:checked").val();
+// var group3 = $("input:radio[name=group3]:checked").val();
+// var group4 = $("input:radio[name=group4]:checked").val();
+// var group5 = $("input:radio[name=group5]:checked").val();
+
+// $(document).ready(function() {
+//   $("form#add").submit(funtion(event) {
+//     event.preventDefault();
+// });
+
+function fQuestion1(){
+  alert("in Question 1");
+}
