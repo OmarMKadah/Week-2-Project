2826c4d5b6f6c59f4e3ebe173a0c4722ba47de4b
Omar Kadah <omarmkadah@gmail.com>
2019-01-27 16:42:39 -0800

    have multiple if/else if statements and new styling class of radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,3 @@
-// var group1 = $("input:radio[name=group1]:checked").val();
-// var group2 = $("input:radio[name=group2]:checked").val();
-// var group3 = $("input:radio[name=group3]:checked").val();
-// var group4 = $("input:radio[name=group4]:checked").val();
-// var group5 = $("input:radio[name=group5]:checked").val();
-
-// $(document).ready(function() {
-//   $("form#add").submit(funtion(event) {
-//     event.preventDefault();
-// });
-
-// function fQuestion1(){
-//   alert("You should learn ________ first!");
-// }
-  // var sCorrectAnswer = document.getElementbyId("ans1");
-
 $(document).ready(function() {
   $(".profile").submit(function(event){
     event.preventDefault();
@@ -23,15 +7,17 @@ $(document).ready(function() {
     var group3Input = $("input:radio[name=exp]:checked").val();
     var group4Input = $("input:radio[name=interest]:checked").val();
     var group5Input = $("input:radio[name=city]:checked").val();
-    // console.log(group1Input);
-    // console.log(group2Input);
-    // console.log(group3Input);
-    // console.log(group4Input);
-    // console.log(group5Input);
 
-    if (group1Input === "art" && group2Input === "app" && group3Input === "little" && group4Input  === "fun" && group5Input === "pdx") {
+
+    if (group1Input === "art" && group2Input === "app" && group3Input === "little" && group4Input  === "fun" && group5Input === "pdx"){
       $(".java").show();
-      
+
+    }  else if (group1Input === "art" || group1Input === "all1" && group2Input === "app" && group3Input === "little" && group4Input  === "fun" && group5Input === "chi"){
+      $(".ruby").show();
+
+    }  else if (group1Input === "math" && group2Input === "app" || group2input === "web" && group3Input === "little" && group4Input  === "fun" && group5Input === "chi"){
+      $(".ccc").show();
+
     }
   });
 })
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Program: “Discover your first Program Language”
 
 README!
 
-Download/clone the respository Week-2-Project to your desktop. Open file index.html in your preferred web browser (author used Google Chrome).
+Download/clone the repository Week-2-Project to your desktop. Open file index.html in your preferred web browser (author used Google Chrome).
 
 **This webpage will educate the user on (5) of the many programming languages used across the world. It includes links to the programs Wikipedia page and enlighten the user on its uses and history. In addition, there is a section that asks a brief, 5 question questionnaire using radio buttons in order to determine the users ideal programming language to begin learning first. Once the (5) questions have been selected, push the “Submit!” button in order to determine your results.
 
